4f73062905994cc2ef9202dccc715754b168ff2b
Abosh Upadhyaya <aboshu@gmail.com>
2022-08-01 23:28:34 -0400

    add `main.py`

---
+++ main.py
@@ -0,0 +1,34 @@
+from typing import NamedTuple
+import csv
+from last_fm import get_top_tracks
+from spotify import get_playlist_id, create_playlist, update_playlist
+
+
+class User(NamedTuple):
+    name: str
+    last_fm_username: str
+    threshold: int
+    moniker: str
+
+
+# Get users from users.csv and create a list of User objects
+users = []
+with open("tables/users.csv", newline="") as f:
+    reader = csv.DictReader(row for row in f if not row.startswith("#"))
+    for row in reader:
+        users.append(
+            User(
+                row["name"],
+                row["last_fm_username"],
+                int(row["threshold"]),
+                row["moniker"],
+            )
+        )
+
+for user in users:
+    get_top_tracks(user)
+    playlist_id = get_playlist_id(user)
+    if playlist_id is None:
+        playlist_id = create_playlist(user)
+
+    update_playlist(playlist_id)
