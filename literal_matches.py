3ffee85e5aa30d12afcde6f62c3afe8ad7ece0b4
Abosh Upadhyaya <aboshu@gmail.com>
2022-08-01 23:28:22 -0400

    add `literal_matches.py`

---
+++ literal_matches.py
@@ -0,0 +1,9 @@
+literal_matches = {
+    ("@ MEH", "Playboi Carti"): "5UusfWUMMLEXLMc1ViNZoe",
+    ("WALK IN THE PARK", "Jack Harlow"): "0XOKietGW4PXK4hs4jyfpO",
+    ("POPSTAR (feat. Drake)", "DJ Khaled"): "1sbEeUY8KsdvgiQi26JBFz",
+    ("Woah", "Lil Baby"): "4P1GuhD2Su9jjyRDPdbeUf",
+    ("Ric Flair Drip (& Metro Boomin)", "Offset"): "7sO5G9EABYOXQKNPNiE9NR",
+    ("Heroes - 1999 Remastered Version", "David Bowie"): "7Jh1bpe76CNTCgdgAdBw4Z",
+    ("童話", "光良"): "6NCylXeJcHOI908PjZDFcg",
+}
