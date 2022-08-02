2725a720ad1c9409300ad6d9ff6685d181cc3518
Abosh Upadhyaya <aboshu@gmail.com>
2022-08-01 23:43:22 -0400

    add `example_secrets.py` as spec for `secrets.py`

---
+++ example_secrets.py
@@ -0,0 +1,27 @@
+'''
+                             _                            
+    _ _    ___    __ _    __| |     o O O  _ __     ___   
+   | '_|  / -_)  / _` |  / _` |    o      | '  \   / -_)  
+  _|_|_   \___|  \__,_|  \__,_|   TS__[O] |_|_|_|  \___|  
+_|"""""|_|"""""|_|"""""|_|"""""| {======|_|"""""|_|"""""| 
+"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'./o--000'"`-0-0-'"`-0-0-' 
+ ______     ______     ______     _____        __    __     ______    
+/\  == \   /\  ___\   /\  __ \   /\  __-.     /\ "-./  \   /\  ___\   
+\ \  __<   \ \  __\   \ \  __ \  \ \ \/\ \    \ \ \-./\ \  \ \  __\   
+ \ \_\ \_\  \ \_____\  \ \_\ \_\  \ \____-     \ \_\ \ \_\  \ \_____\ 
+  \/_/ /_/   \/_____/   \/_/\/_/   \/____/      \/_/  \/_/   \/_____/ 
+                                                                      
+⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
+RENAME THIS FILE TO secrets.py FOR THE API KEYS TO WORK!
+
+Then, fill out your API information below.
+'''
+
+# Last.fm API
+LAST_FM_API_KEY = "YOUR_LAST_FM_API_KEY_HERE"
+LAST_FM_SHARED_SECRET = "YOUR_LAST_FM_SHARED_SECRET_HERE"
+
+# Spotify API and username
+SPOTIFY_CLIENT_ID = "YOUR_SPOTIFY_CLIENT_ID_HERE"
+SPOTIFY_CLIENT_SECRET = "YOUR_SPOTIFY_CLIENT_SECRET_HERE"
+SPOTIFY_USERNAME = "YOUR_SPOTIFY_USERNAME_HERE"
