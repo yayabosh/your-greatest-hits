c660e1e3d887e064f1ba2c2268de0f051bf4cfcb
Abosh Upadhyaya <aboshu@gmail.com>
2022-08-02 00:21:25 -0400

    document `main.py`

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-import csv
-from typing import NamedTuple
+import csv  # For parsing `tables/users.csv``
+from typing import NamedTuple  # For creating a named tuple representing a user
 
-from last_fm import get_top_tracks
-from spotify import create_playlist, get_playlist_id, update_playlist
+from last_fm import get_top_tracks  # For getting the top tracks for a user
+from spotify import (
+    create_playlist,
+    get_playlist_id,
+    update_playlist,
+)  # For creating and updating a playlist
 
 
+# A named tuple representing a user in the `users.csv` table
 class User(NamedTuple):
-    name: str
-    last_fm_username: str
-    threshold: int
-    moniker: str
+    name: str  # Example: "abosh"
+    last_fm_username: str  # Example: "yayabosh"
+    threshold: int  # Example: 100
+    moniker: str  # Example: "Abosh Has", so the Spotify playlist will be named "Songs {Abosh Has} Listened To 100+ Times"
 
 
 # Get users from users.csv and create a list of User objects
 users = []
 with open("tables/users.csv", newline="") as f:
+    # Parse each line in `users.csv`, ignoring lines that start with a #
     reader = csv.DictReader(row for row in f if not row.startswith("#"))
     for row in reader:
+        # Create a User object for each row in `users.csv` and add it to the users list
         users.append(
             User(
                 row["name"],
@@ -26,10 +33,16 @@ with open("tables/users.csv", newline="") as f:
             )
         )
 
+# For each user in the users list, get their top tracks (tracks above their
+# threshold) from Last.fm and create a playlist for them in Spotify
 for user in users:
+    # Get the top tracks for the user
     get_top_tracks(user)
+    # Get the playlist ID for the user's playlist, if it exists
     playlist_id = get_playlist_id(user)
+    # If the playlist doesn't exist, create it
     if playlist_id is None:
         playlist_id = create_playlist(user)
 
+    # Update the playlist with the top tracks for the user
     update_playlist(playlist_id)
