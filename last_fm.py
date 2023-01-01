0ac8ba83404215cc98c0de4c44fb379d17adf89c
Abosh Upadhyaya <aboshu@gmail.com>
2022-12-31 22:59:37 -0800

    Actually use an algorithm to query tracks instead of just hardcoding it

--- last_fm.py
+++ last_fm.py
@@ -2,11 +2,6 @@ import requests  # For calling the Last.fm API
 
 from secret import LAST_FM_API_KEY
 
-# Number of pages to search through a user's top tracks. This number is
-# arbitrary, but it works so far, so I'll update it later.
-NUM_PAGES = 4
-
-
 # Populates songs.csv with songs played more than the given threshold.
 # So if the threshold is 100, songs.csv will contain all songs (and their
 # respective artists) played more than 100 times.
@@ -19,19 +14,30 @@ def get_tracks_above_threshold(user, threshold):
         "user": user.last_fm_username,
         "format": "json",
     }
-    # Stores all songs in the user's first NUM_PAGES of top tracks.
-    pages = []
-    # Call the Last.fm API NUM_PAGES times and add the results to pages.
-    for p in range(1, NUM_PAGES + 1):
-        last_fm_payload["page"] = p
-        r = requests.get("https://ws.audioscrobbler.com/2.0/", params=last_fm_payload)
-        pages.append(r.json()["toptracks"]["track"])
 
-    # Add all songs played more than the given threshold to songs.csv.
+    # Clear the songs.csv file
     with open("tables/songs.csv", "w") as f:
         f.write("song,artist\n")
-        for page in pages:
+
+    # Keep calling the Last.fm API while the songs in the last page are played
+    # at least threshold number of times. Once we reach a page where the songs
+    # are played less than threshold number of times, we can stop.
+    page_number = 1  # The page number of the user's scrobbles to retrieve
+    while True:
+        # Call the Last.fm API and store the result.
+        last_fm_payload["page"] = page_number
+        r = requests.get("https://ws.audioscrobbler.com/2.0/", params=last_fm_payload)
+        page = r.json()["toptracks"]["track"]
+
+        # Append all songs played more than the given threshold to songs.csv.
+        with open("tables/songs.csv", "a") as f:
             for song in page:
                 # This filters songs played less than the given threshold.
                 if int(song["playcount"]) >= threshold:
                     f.write(f'"{song["name"]}","{song["artist"]["name"]}"\n')
+                else:
+                    # If we reach a page that contains a song played less than
+                    # the given threshold, we can stop calling the Last.fm API.
+                    return
+
+        page_number += 1
